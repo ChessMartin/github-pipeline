c58bd1641caa7a4a956421547172b7e53a5ca343
ChessMartin <martin.le.formal@efrei.net>
2024-01-18 20:31:16 +0100

    this is the app

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
+todos = []
 
-@app.route('/')
-def home():
-    return 'Welcome to my simple Flask app!'
+@app.route('/', methods=['GET', 'POST'])
+def todo():
+    if request.method == 'POST':
+        todo_item = request.form.get('todo_item')
+        if todo_item:
+            todos.append(todo_item)
+
+    return render_template_string("""
+        <!doctype html>
+        <html>
+            <head>
+                <title>To-Do List</title>
+            </head>
+            <body>
+                <h1>To-Do List</h1>
+                <form method="post">
+                    <input type="text" name="todo_item" />
+                    <input type="submit" value="Add"/>
+                </form>
+                <ul>
+                    {% for todo in todos %}
+                        <li>{{ todo }}</li>
+                    {% endfor %}
+                </ul>
+            </body>
+        </html>
+    """, todos=todos)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(host='0.0.0.0', port=80)
